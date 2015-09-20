b252a852ed59c6eeaadc78909d678eae72028777
Kareem Janoudi <kjanoudi@gmail.com>
2015-09-20 14:37:50 +0300

    remove content ended listener

--- src/videojs.ima.js
+++ src/videojs.ima.js
@@ -969,6 +969,7 @@
     };
 
     var playerDisposedListener = function(){
+      player.off('ended', localContentEndedListener);
       if(updateTimeIntervalHandle){
         clearInterval(updateTimeIntervalHandle);
       }
