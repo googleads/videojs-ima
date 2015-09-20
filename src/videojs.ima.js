c8cabf26359d526c1e23f2586511f4c4d4fd560c
Kareem Janoudi <kjanoudi@gmail.com>
2015-09-20 08:10:44 +0300

    clear timeouts and intervals when player is disposed/reset

--- src/videojs.ima.js
+++ src/videojs.ima.js
@@ -1092,7 +1092,25 @@
       clearInterval(updateTimeIntervalHandle);
       clearInterval(seekCheckIntervalHandle);
       clearInterval(resizeCheckIntervalHandle);
-      player.one('play', player.ima.setUpPlayerIntervals_);
+      if(player.el()) player.one('play', player.ima.setUpPlayerIntervals_);
+    };
+
+    var playerDisposedListener = function(){
+      contentEndedListeners, contentAndAdsEndedListeners = [], [];
+      contentComplete = true;
+      player.off('ended', localContentEndedListener);
+      var intervalsToClear = [updateTimeIntervalHandle, seekCheckIntervalHandle,
+        adTrackingTimer, resizeCheckIntervalHandle];
+      for (var index in intervalsToClear) {
+        var interval = intervalsToClear[index];
+        if (interval) {
+          clearInterval(interval);
+        }
+      }
+      if (adsManager) {
+        adsManager.destroy();
+        adsManager = null;
+      }
     };
 
     settings = extend({}, ima_defaults, options || {});
@@ -1118,6 +1136,7 @@
     player.one('play', player.ima.setUpPlayerIntervals_);
 
     player.on('ended', localContentEndedListener);
+    player.on('dispose', playerDisposedListener);
 
     var contrib_ads_defaults = {
       debug: settings.debug,
