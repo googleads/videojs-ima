be2ef87720d16efc837a7f7be3d8b5b8571b4ac2
Aydın Akan <aydin.kn12@gmail.com>
2016-01-05 09:44:06 +0200

    Hide ad controls for not video ads (like modal)

--- src/videojs.ima.js
+++ src/videojs.ima.js
@@ -305,7 +305,13 @@
         player.ads.startLinearAdMode();
       }
       adContainerDiv.style.display = 'block';
-      controlsDiv.style.display = 'block';
+      // Don't show ad controls for not video ads (like modal ads)
+      if (adEvent.getAd().getContentType().search(/video/i) !== 0) {
+        controlsDiv.style.display = 'none';
+      }
+      else {
+        controlsDiv.style.display = 'block';
+      }
       vjsControls.hide();
       player.pause();
     };
