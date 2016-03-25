a895d5a25061ef1abb1e62bb0627df57e121c7ed
Shawn Busolits <sbusolits@sbusolits-macbookpro3.roam.corp.google.com>
2016-03-25 16:29:33 -0400

    Added a customizable ad label field to the settings.

--- src/videojs.ima.js
+++ src/videojs.ima.js
@@ -37,7 +37,8 @@
   ima_defaults = {
     debug: false,
     timeout: 5000,
-    prerollTimeout: 100
+    prerollTimeout: 100,
+    adLabel: 'Advertisement'
   },
 
   imaPlugin = function(options, readyCallback) {
@@ -88,7 +89,7 @@
       controlsDiv.style.width = '100%';
       countdownDiv = document.createElement('div');
       countdownDiv.id = 'ima-countdown-div';
-      countdownDiv.innerHTML = 'Advertisement';
+      countdownDiv.innerHTML = settings.adLabel;
       countdownDiv.style.display = showCountdown ? 'block' : 'none';
       seekBarDiv = document.createElement('div');
       seekBarDiv.id = 'ima-seek-bar-div';
@@ -419,7 +420,7 @@
         podCount = ' (' + adPosition + ' of ' + totalAds + '): ';
       }
       countdownDiv.innerHTML =
-          'Advertisement' + podCount +
+          settings.adLabel + podCount +
           remainingMinutes + ':' + remainingSeconds;
 
       // Update UI
