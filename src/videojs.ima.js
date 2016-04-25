91fabec43b3cb63dfd0daf9376dd2887b31cd45d
Hendra Uzia <hendra.uzia@gmail.com>
2016-04-26 02:24:39 +0700

    Add data from AdError and AdErrorEvent on adserror (#217)

--- src/videojs.ima.js
+++ src/videojs.ima.js
@@ -256,7 +256,7 @@
       if (adsManager) {
         adsManager.destroy();
       }
-      player.trigger('adserror');
+      player.trigger({type: 'adserror', data: { AdError: event.getError(), AdErrorEvent: event }});
     };
 
     /**
@@ -270,7 +270,7 @@
       vjsControls.show();
       adsManager.destroy();
       adContainerDiv.style.display = 'none';
-      player.trigger('adserror');
+      player.trigger({ type: 'adserror', data: { AdError: adErrorEvent.getError(), AdErrorEvent: adErrorEvent }});
     };
 
     /**
