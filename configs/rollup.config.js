6a7cdb1f6b7a6bc5407e4d103e6175d56e1dcf4e
mimse <movh@tv2.dk>
2018-01-03 13:03:47 +0100

    Added babel to build for ES2015, so older browsers are supported

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -1,21 +1,25 @@
 import copy from 'rollup-plugin-copy';
-import json from 'rollup-plugin-json'
+import json from 'rollup-plugin-json';
+import babel from 'rollup-plugin-babel';
 
 export default {
   name: 'videojsIma',
   input: 'src/ima-plugin.js',
   output: {
     file: 'dist/videojs.ima.js',
-    format: 'umd'
+    format: 'umd',
   },
   external: ['video.js', 'videojs-contrib-ads'],
   globals: {
-    'video.js': 'videojs'
+    'video.js': 'videojs',
   },
   plugins: [
     json(),
     copy({
-      "src/css/videojs.ima.css": "dist/videojs.ima.css",
-    })
-  ]
+      'src/css/videojs.ima.css': 'dist/videojs.ima.css',
+    }),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
