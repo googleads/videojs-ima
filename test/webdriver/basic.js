c51e469996ebea517eec4f558be31371ac4e1c7f
Yury Pavlotsky <pavlotsky@google.com>
2018-02-02 16:26:42 -0500

    removed sleep

--- test/webdriver/basic.js
+++ test/webdriver/basic.js
@@ -44,9 +44,8 @@ test.describe('Basic Tests', function() {
     });
 
      test.it( 'Displays title ' + browser.name, function(){
-      driver.get('https://googleads.github.io/videojs-ima/examples/simple/');
+      driver.get('http://localhost:8080/test/webdriver/index.html?ad=linear');
       driver.findElement(By.id('content_video')).click();
-      driver.sleep(1000);
       driver.takeScreenshot().then(function(image, err) {
         console.log(image);
       });
