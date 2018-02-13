8f2a98233653e18efedbbfbea5dd9f2932422fb6
Shawn Busolits <shawn.busolits@gmail.com>
2018-02-13 17:06:02 -0500

    fix: Fix preversion script.

--- scripts/preversion.js
+++ scripts/preversion.js
@@ -1,4 +1,4 @@
 if (process.env.BROWSERSTACK_USER === undefined ||
-    process.env.BROWSERSTACK_KEY === undefined) {
+    process.env.BROWSERSTACK_ACCESS_KEY === undefined) {
   process.exit(1);
 }
