d6b6b2817e17461b21f27ccef03d023482f99574
Shawn Busolits <shawn.busolits@gmail.com>
2018-02-14 09:43:03 -0500

    Add logging line to preversion script on fail. (#517)

--- scripts/preversion.js
+++ scripts/preversion.js
@@ -1,4 +1,6 @@
 if (process.env.BROWSERSTACK_USER === undefined ||
     process.env.BROWSERSTACK_ACCESS_KEY === undefined) {
+  console.error(
+      'ERROR: Both BROWSERSTACK_USER and BROWSERSTACK_ACCESS_KEY must be set');
   process.exit(1);
 }
