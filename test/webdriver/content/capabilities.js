f7d2d14731b90b7f082c594b88c54dd38d93836e
Yury Pavlotsky <pavlotsky@google.com>
2018-02-14 14:49:30 -0500

    test: removed pull request check

--- test/webdriver/content/capabilities.js
+++ test/webdriver/content/capabilities.js
@@ -75,13 +75,10 @@ for (let browser of browsers) {
 }
 
 // Remove if we don't have browserstack credentials.
-if (process.env.TRAVIS_PULL_REQUEST === false||
-    process.env.BROWSERSTACK_USER === undefined ||
+if (process.env.BROWSERSTACK_USER === undefined ||
     process.env.BROWSERSTACK_ACCESS_KEY === undefined) {
   browsers = browsers.filter(browser =>
     browser.server != 'http://hub-cloud.browserstack.com/wd/hub');
 }
-
-console.log('TRAVIS_PULL_REQUEST:' + process.env.TRAVIS_PULL_REQUEST);
 
 exports.browsers = browsers;
