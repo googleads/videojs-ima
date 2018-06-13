80aba35c37c385e8e98fb1395f705a717c0aea7d
Shawn Busolits <shawn.busolits@gmail.com>
2018-06-13 14:57:22 -0400

    feat: Allow for an adsRequest object to be passed to the plugin. This… (#656)
    * feat: Allow for an adsRequest object to be passed to the plugin. This allows us to support additional adRequest properties. Fixes #653.
    
    * Updated to accept adsRequest everywhere we accept adTagUrl or adResponse.
    
    * Fix missing null check.
    
    * Fix typo
    
    * Added setContentWithAdsRequest to ima-plugin.
    
    * Fix bad copypasta.
    
    * Fix lint error.

--- src/sdk-impl.js
+++ src/sdk-impl.js
@@ -60,11 +60,6 @@ const SdkImpl = function(controller) {
   this.adsRenderingSettings = null;
 
   /**
-   * Ad tag URL. Should return VAST, VMAP, or ad rules.
-   */
-  this.adTagUrl = null;
-
-  /**
    * VAST, VMAP, or ad rules response. Used in lieu of fetching a response
    * from an ad tag URL.
    */
@@ -218,6 +213,15 @@ SdkImpl.prototype.requestAds = function() {
       this.controller.getPlayerHeight();
   adsRequest.setAdWillAutoPlay(this.controller.adsWillAutoplay());
   adsRequest.setAdWillPlayMuted(this.controller.adsWillPlayMuted());
+
+  // Populate the adsRequestproperties with those provided in the AdsRequest
+  // object in the settings.
+  let providedAdsRequest = this.controller.getSettings().adsRequest;
+  if (providedAdsRequest && typeof providedAdsRequest === 'object') {
+    Object.keys(providedAdsRequest).forEach((key) => {
+      adsRequest[key] = providedAdsRequest[key];
+    });
+  }
 
   this.adsLoader.requestAds(adsRequest);
   this.controller.triggerPlayerEvent('ads-request', adsRequest);
