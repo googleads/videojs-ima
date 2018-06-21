59910b20eddb6944ee61760081a5cb76ba2d45a0
Yury Pavlotsky <pavlotsky@google.com>
2018-06-21 14:42:05 -0400

    remove code

--- examples/simple/ads.js
+++ examples/simple/ads.js
@@ -25,11 +25,6 @@ var options = {
       'vid=short_onecue&correlator='
 };
 
-player.on('ads-request', function(event, data) {
-  console.log('ads-request:' + event.type + " " + data);
-});
-player.ima(options);
-
 // Remove controls from the player on iPad to stop native controls from stealing
 // our click
 var contentPlayer =  document.getElementById('content_video_html5_api');
